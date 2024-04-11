99f7ba00c20f98373acd3e66153b7022517a1a3e
vikenty19 <vikenty.plakhov@testpro.io>
2024-04-11 15:56:31 -0400

    api manager is initializes

--- src/main/java/api/api_manager/ApiManager.java
+++ src/main/java/api/api_manager/ApiManager.java
@@ -1,4 +1,11 @@
 package api.api_manager;
 
+import lombok.Getter;
+import lombok.Setter;
+import models.google_places.GooglePlacesModel;
+
 public class ApiManager {
+    @Setter
+    @Getter
+    GooglePlacesModel googlePlacesModel = new GooglePlacesModel();
 }
